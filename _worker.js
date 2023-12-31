0f1e7f7eded61873e5c9815faca4fbb45df94302
HDJN <17902735+HDJN@users.noreply.github.com>
2024-01-01 00:01:44 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'b953b60c-f1c0-4935-96f0-ee63984fdea3';
+let userID = '1bfac185-f1c1-48c4-a2e9-f971934ea9bb';
 
 const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
